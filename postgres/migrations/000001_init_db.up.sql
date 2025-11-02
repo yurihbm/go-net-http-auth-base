5ca2b63b4d0aa0a9eb0df503d9616a68d7d7ba0c
Yuri Henrique Bernardes Maciel <35900428+yurihbm@users.noreply.github.com>
2025-11-02 15:55:21 -0300

    chore: format sql code

--- postgres/migrations/000001_init_db.up.sql
+++ postgres/migrations/000001_init_db.up.sql
@@ -5,13 +5,13 @@ CREATE TABLE users (
     name text NOT NULL,
     email text UNIQUE NOT NULL,
     password_hash text,
-    created_at timestamptz DEFAULT NOW() NOT NULL,
-    updated_at timestamptz DEFAULT NOW() NOT NULL
+    created_at timestamptz DEFAULT now() NOT NULL,
+    updated_at timestamptz DEFAULT now() NOT NULL
 );
 
-CREATE OR REPLACE FUNCTION update_updated_at_column ()
-    RETURNS TRIGGER
-    AS $$
+CREATE OR REPLACE FUNCTION update_updated_at_column()
+RETURNS trigger
+AS $$
 BEGIN
     NEW.updated_at = now();
     RETURN NEW;
@@ -20,9 +20,9 @@ $$
 LANGUAGE 'plpgsql';
 
 CREATE TRIGGER update_users_updated_at
-    BEFORE UPDATE ON users
-    FOR EACH ROW
-    EXECUTE PROCEDURE update_updated_at_column ();
+BEFORE UPDATE ON users
+FOR EACH ROW
+EXECUTE PROCEDURE update_updated_at_column();
 
 CREATE TYPE oauth_provider AS ENUM (
     'google'
@@ -30,11 +30,10 @@ CREATE TYPE oauth_provider AS ENUM (
 
 CREATE TABLE user_oauth_providers (
     uuid uuid DEFAULT uuidv7() NOT NULL UNIQUE PRIMARY KEY,
-    user_uuid uuid REFERENCES users(uuid) ON DELETE CASCADE,
+    user_uuid uuid REFERENCES users (uuid) ON DELETE CASCADE,
     provider oauth_provider NOT NULL,
     provider_user_id text NOT NULL,
     provider_email text NOT NULL,
-    created_at timestamptz DEFAULT NOW() NOT NULL,
+    created_at timestamptz DEFAULT now() NOT NULL,
     UNIQUE (provider, provider_user_id)
 );
-
