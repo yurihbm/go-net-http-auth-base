359c3a0d2505354b4a2ca15bfb197093719cb2c2
Yuri Henrique B. Maciel <35900428+yurihbm@users.noreply.github.com>
2026-01-28 22:08:36 -0300

    feat: add audit domain

---
+++ internal/domain/audit.go
@@ -0,0 +1,37 @@
+package domain
+
+type AuditLog struct {
+	UUID          string  `json:"uuid"`
+	ActorUUID     *string `json:"actor_uuid,omitempty"`
+	IPAddress     string  `json:"ip_address"`
+	UserAgent     string  `json:"user_agent"`
+	Action        string  `json:"action"`
+	ResourceType  string  `json:"resource_type"`
+	ResourceUUID  string  `json:"resource_uuid"`
+	RequestUUID   string  `json:"request_uuid"`
+	Changes       any     `json:"changes,omitempty"`
+	Status        string  `json:"status"`
+	FailureReason *string `json:"failure_reason,omitempty"`
+	CreatedAt     int64   `json:"created_at"`
+}
+
+type CreateAuditLogDTO struct {
+	ActorUUID     *string
+	IPAddress     string
+	UserAgent     string
+	Action        string
+	ResourceType  string
+	ResourceUUID  string
+	RequestUUID   string
+	Changes       any
+	Status        string
+	FailureReason *string
+}
+
+type AuditService interface {
+	Log(CreateAuditLogDTO) error
+}
+
+type AuditRepository interface {
+	Create(*AuditLog) error
+}
